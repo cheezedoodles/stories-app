c807ba55a9013d22fccc8bd9f6b835e6b8f2b242
cheezedoodles <hougvapes@gmail.com>
2023-01-01 19:16:41 +0300

    created search component

--- stories/src/App.js
+++ stories/src/App.js
@@ -22,16 +22,12 @@ const App = () => {
     },
   ];
 
-  const handleChange = event => {
-    console.log(event.target.value);
-  }
 
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={handleChange}/>
+     <Search />
 
       <hr />
 
@@ -39,6 +35,27 @@ const App = () => {
     </div>
   );
 }
+
+
+const Search = () => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleChange = event => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handleChange} />
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
+    </div>
+  );
+};
+
 
 
 const List = props => (
