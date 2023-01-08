09751a927d9c765ff52742fa67502978687aabf8
cheezedoodles <hougvapes@gmail.com>
2023-01-08 19:53:54 +0300

    used useRef to autofocus an input field

--- stories/src/App.js
+++ stories/src/App.js
@@ -54,9 +54,12 @@ const App = () => {
       <InputWithLabel
         id="search"
         label="Search" 
-        value={searchTerm} 
+        value={searchTerm}
+        isFocused 
         onInputChange={handleSearch} 
-      />
+      >
+        Search
+      </InputWithLabel>
 
       <p>{searchTerm}</p>
 
@@ -67,18 +70,37 @@ const App = () => {
 }
 
 
-const InputWithLabel = ({ id, label, value, type = 'text', onInputChange }) => (
-    <>
-      <label htmlFor={id}>{label}</label>
-      &nbsp;
-      <input
-        id={id}
-        type={type}
-        value={value}
-        onChange={onInputChange}
-      />
-    </>
-)
+const InputWithLabel = ({ 
+  id, 
+  value, 
+  type = 'text', 
+  onInputChange, 
+  isFocused,
+  children, 
+}) => {
+
+    const inputRef = React.useRef();
+
+    React.useEffect(() => {
+      if (isFocused && inputRef.current) {
+        inputRef.current.focus();
+      }
+    }, [isFocused])
+
+    return (
+      <>
+        <label htmlFor={id}>{children}</label>
+        &nbsp;
+        <input
+          ref={inputRef}
+          id={id}
+          type={type}
+          value={value}
+          onChange={onInputChange}
+        />
+      </>
+    );
+}
 
 
 const List = ({ list }) => 
