ab6a5e8e4786b6ea7f259859d955aa5eeaf402aa
cheezedoodles <hougvapes@gmail.com>
2023-01-12 19:23:35 +0300

    using useCallback to fetchdata

--- stories/src/App.js
+++ stories/src/App.js
@@ -59,8 +59,7 @@ const App = () => {
     { data: [], isLoading: false, isError: false }
   );
 
-  React.useEffect(() => {
-    
+  const handleFetchStories = React.useCallback(() => {
     if (!searchTerm) return;
     
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
@@ -74,7 +73,11 @@ const App = () => {
         });
       })
       .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
-  }, [searchTerm]);
+  }, [searchTerm])
+
+  React.useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
 
   const handleRemoveStory = (item) => {
     dispatchStories({
