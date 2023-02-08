68df447a1eb01e1d9111431a5d176706deecdc1c
cheezedoodles <hougvapes@gmail.com>
2023-02-08 18:59:52 +0300

    updated dependencies && setup.js for testing

--- stories/vite.config.ts
+++ stories/vite.config.ts
@@ -5,4 +5,8 @@ import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [react(), svgr()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: './tests/setup.js',
+  },
 });
